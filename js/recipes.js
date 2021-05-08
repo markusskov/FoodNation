224fec67e5f31154e573fcf84aef762b487a4d96
Markus Skov <Markusskov@hotmail.no>
2021-05-09 00:10:25 +0200

    Started on recipes page

---
+++ js/recipes.js
@@ -0,0 +1,25 @@
+const url = 'https://api.markusskov.tech/wp-json/wp/v2/posts?_embed';
+
+const container = document.getElementById('recipes');
+
+async function getRecipes() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const result = data;
+    console.log(result);
+    for (let i = 0; i < result.length; i++) {
+      container.innerHTML += `
+    <div class="cards">
+        <div class="card">
+            <a href="details.html?id=${result[i].id}"><img class="recipe-img" src="${result[i]._embedded['wp:featuredmedia'][0].source_url}"/></a>
+            <a class="card-text" href="details.html?id=${result[i].id}">${result[i].title.rendered}</a>
+        </div>
+    </div>
+        `;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getRecipes();
