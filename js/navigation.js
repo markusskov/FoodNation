e0b37e628996748c22cd959de4c01254246c9f88
Markus Skov <Markusskov@hotmail.no>
2021-05-18 20:32:37 +0200

    Changed a bit more on mobile navigation

--- js/navigation.js
+++ js/navigation.js
@@ -2,9 +2,9 @@ const hamburger = document.getElementById('hamburger');
 const mobileContainer = document.querySelector('.mobile__container');
 
 hamburger.addEventListener('click', function (event) {
-  if (mobileContainer.style.display === 'none') {
-    mobileContainer.style.display = 'flex';
-  } else {
+  if (mobileContainer.style.display === 'flex') {
     mobileContainer.style.display = 'none';
+  } else {
+    mobileContainer.style.display = 'flex';
   }
 });
